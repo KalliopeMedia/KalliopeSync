d28549b2ff9eb5946f03196e5914dec502b806e7
Sumit Kumar Maitra <sumitkm@gmail.com>
2015-11-15 01:19:40 +0000

    Work in progress on connection window

---
+++ KalliopeSync.Gtk/Views/ConnectionDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KalliopeSync.View
+{
+    public partial class ConnectionDialog : Gtk.Dialog
+    {
+        public ConnectionDialog()
+        {
+            this.Build();
+        }
+    }
+}
+
---
+++ KalliopeSync.Gtk/Views/MainWindow.cs
@@ -0,0 +1,65 @@
+﻿using Gtk;
+
+using System;
+using System.Collections.Generic;
+
+namespace KalliopeSync.View
+{
+    public partial class MainWindow: Gtk.Window
+    {
+        public MainWindow()
+            : base(Gtk.WindowType.Toplevel)
+        {
+            Build();
+
+            SyncItem root = new SyncItem{ Id = DateTime.Now.Ticks, FileKey = "/root", Name = "Root" };
+            for (int j = 0; j < 5; j++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    string fileKey = "/root" + string.Format("/file/path/{0}/{1}{2}", i, string.Format("FileItem {0}-", i), j); 
+                    KalliopeSync.Core.Models.IndexItem subItem = new KalliopeSync.Core.Models.IndexItem
+                    {
+                        Name = string.Format("FileItem {0}", i),
+                        Id = fileKey
+                    };      
+                    root.AddItem(subItem.Id, subItem);
+                }
+            }
+
+            var syncItems = this.SyncLocationTreeView.DataBind(root);
+
+            Gtk.TreeViewColumn folderNameColumn = new TreeViewColumn();
+            folderNameColumn.Title = "Folders";
+            Gtk.CellRendererText folderNameCell = new Gtk.CellRendererText();
+            folderNameColumn.PackStart(folderNameCell, true);
+            folderNameColumn.SetCellDataFunc(folderNameCell, new Gtk.TreeCellDataFunc(RenderFolderNameCell));
+
+            this.SyncLocationTreeView.AppendColumn(folderNameColumn);
+            this.SyncLocationTreeView.Model = syncItems;
+        }
+
+        private void RenderFolderNameCell(Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
+        {
+            SyncItem syncItem = (SyncItem)model.GetValue(iter, 0);
+            (cell as Gtk.CellRendererText).Text = syncItem.Name;
+
+        }
+
+        protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+            a.RetVal = true;
+        }
+        protected void OnConnectActionActionChanged (object o, ChangedArgs args)
+        {
+            throw new NotImplementedException ();
+        }
+
+
+        protected void OnConnectActionActionChangedPataNahin (object o, ChangedArgs args)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
